c82a86afd3793657989d58bcf13eea5e6f9c1d5d
Tonazar <70108985+Tonazar@users.noreply.github.com>
2020-08-26 16:17:56 +0400

    bug fix

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from "react";
 
+const seasonConfig = {
+  Summer: {
+    text: "Let's hit the beach!",
+    iconName: "sun",
+  },
+  Winter: {
+    text: "Burr, it is cold!",
+    iconName: "snowflake",
+  },
+};
+
 const getSeason = (lat, month) => {
   if (month > 0 && month < 9) {
     return lat > 0 ? "Summer" : "Winter";
@@ -10,9 +21,18 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-  const text = season === "Winter" ? "Burr, its chilly" : "Lets hit the beach";
+  // const text = season === "Winter" ? "Burr, its chilly" : "Lets hit the beach";
+  // const icon = season === "Winter" ? "snowflake" : "sun";
 
-  return <div>{}</div>;
+  const { text, iconName } = seasonConfig[season];
+
+  return (
+    <div>
+      <i className={`${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`${iconName} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ class App extends React.Component {
     this.state = {
       lat: null,
       errorMessage: "",
-      hi: "",
     };
 
     window.navigator.geolocation.getCurrentPosition(
